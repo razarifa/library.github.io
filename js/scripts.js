091e6083d5ccc01946b7151606bea8f6f24bf296
Zarifa Rasullu <zerife.resullu@gmail.com>
2021-06-23 18:03:56 +0400

    Almost works

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,5 @@
 //variables
-let myLibrary = [
- {
-  status: "willRead",
-  books: [],
- },
- {
-  status: "isReading",
-  books: [],
- },
- {
-  status: "haveRead",
-  books: [],
- },
-];
+let books = [];
 const add = document.querySelector("#add");
 
 //constructors
@@ -25,73 +12,100 @@ function Book(title, author, pages, readStatus) {
   return `${this.title} by ${this.author}, ${this.pages}, ${readStatus} `;
  };
 }
-Book.prototype.changeStatus = function (status) {
- this.readStatus = status;
-};
+//events
+document.querySelector("form").addEventListener("submit", addBookToLibrary);
+
 //functions
-function addBookToLibrary(book) {
- myLibrary.forEach((lib) => {
-  if (lib.status === book.readStatus) {
-   lib.books.push(book);
-   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-   location.reload();
-  }
- });
-}
-//
-add.addEventListener("click", () => {
+function addBookToLibrary(e) {
+ e.preventDefault();
  let formTitle = document.querySelector("#title").value;
  let formAuthor = document.querySelector("#author").value;
  let formPages = document.querySelector("#pages").value;
  let formReadStatus = document.querySelector("#readingStatus").value;
- addBookToLibrary(new Book(formTitle, formAuthor, formPages, formReadStatus));
-});
+ const book = new Book(formTitle, formAuthor, formPages, formReadStatus);
+ books.push(book);
+ localStorage.setItem("books", JSON.stringify(books));
+ render();
+ this.reset();
+}
 
-if (localStorage.getItem("myLibrary") !== null) {
- myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-
- myLibrary.forEach((lib) => {
+function render() {
+ if (localStorage.getItem("books") !== null) {
+  books = JSON.parse(localStorage.getItem("books"));
+ }
+ document.querySelector(`#willRead`).innerHTML = "";
+ document.querySelector(`#isReading`).innerHTML = "";
+ document.querySelector(`#haveRead`).innerHTML = "";
+ books.forEach((book, index) => {
   if (
-   lib.status === document.querySelector(`#${lib.status}`).getAttribute("id")
+   book.readStatus ===
+   document.querySelector(`#${book.readStatus}`).getAttribute("id")
   ) {
-   lib.books.forEach((book) => {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    document.querySelector(`#${lib.status}`).appendChild(card);
-    card.innerHTML = `
-     <p><b>Title:</b>${book.title}</p>
-     <p><b>Author: </b>${book.author}</p>
-     <p><b>Page Count:</b> ${book.pages}</p>
-     <div class="button-container">
-      <button class="delete">Delete</button>
-      </div>
-      <select name="readingStatus" class="statusSelect" >
-      <option value="willRead">Want to read</option>
-      <option value="isReading">In Progress</option>
-      <option value="haveRead">I have already read</option>
-     </select>
-     `;
-    [...card.querySelector(".statusSelect")].forEach((s) => {
-     if (s.value === book.readStatus) {
-      s.setAttribute("selected", "selected");
-     }
-    });
+   let card = document.createElement("div");
+   card.classList.add("card");
+   document.querySelector(`#${book.readStatus}`).appendChild(card);
+   card.innerHTML = `
+        <p><b>Title:</b>${book.title}</p>
+        <p><b>Author: </b>${book.author}</p>
+        <p><b>Page Count:</b> ${book.pages}</p>
+        <div class="button-container">
+         <button class="delete">Delete</button>
+         </div>
+         <select name="readingStatus" class="statusSelect" >
+         <option value="willRead" class="choice">Want to read</option>
+         <option value="isReading" class="choice">In Progress</option>
+         <option value="haveRead" class="choice">I have already read</option>
+        </select>
+        `;
+   [...card.querySelector(".statusSelect")].forEach((s) => {
+    if (s.value === book.readStatus) {
+     s.setAttribute("selected", "selected");
+    }
    });
   }
  });
 }
-[...document.querySelectorAll(".delete")].forEach((del) => {
- del.addEventListener("click", () => {
-  let attribute =
-   del.parentElement.parentElement.parentElement.getAttribute("id");
-  del.parentElement.parentElement.parentElement.removeChild(
-   del.parentElement.parentElement
-  );
-  myLibrary.forEach((lib) => {
-   if (lib.status === attribute) {
-    lib.books.pop();
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-   }
-  });
+
+function removeCard(e) {
+ //delete from array
+ books.map((book) => {
+  if (
+   e.parentElement.parentElement.children[0].innerText.substr(6).toString() ===
+   book.title
+  ) {
+   books.splice(books.indexOf(book), 1);
+  }
+ });
+ //delete from dom
+ e.parentElement.parentElement.parentElement.removeChild(
+  e.parentElement.parentElement
+ );
+
+ localStorage.setItem("books", JSON.stringify(books));
+}
+function changeStatus(e) {
+ books.map((book) => {
+  if (
+   e.parentElement.children[0].innerText.substr(6).toString() === book.title
+  ) {
+   book.readStatus = e.options[e.options.selectedIndex].value;
+   localStorage.setItem("books", JSON.stringify(books));
+   render();
+  }
+ });
+}
+[...document.querySelectorAll(".list")].forEach((list) => {
+ list.addEventListener("click", (e) => {
+  if (e.target.matches(".delete")) {
+   removeCard(e.target);
+  }
  });
 });
+[...document.querySelectorAll(".list")].forEach((list) => {
+ list.addEventListener("change", (e) => {
+  if (e.target.matches(".statusSelect")) {
+   changeStatus(e.target);
+  }
+ });
+});
+render();
